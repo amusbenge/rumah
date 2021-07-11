16cfd7898edd91e8ced59d7b1985b69bcad78964
amosbenge <60687146+amosbenge@users.noreply.github.com>
2021-07-11 14:53:43 +0800

    baru

--- application/views/auth.php
+++ application/views/auth.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <link rel="icon" type="image/png" href="<?= base_url('assets/img/icon/rumah.png') ?>">
+    <link rel="icon" type="image/png" href="<?= base_url('assets/img/icon/house.png') ?>">
     <title><?= $title; ?></title>
 
     <!-- Custom fonts for this template-->
@@ -18,6 +18,8 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- css ex -->
     <link href="<?= base_url('assets/'); ?>css/private.css" rel="stylesheet">
 
 </head>
@@ -29,36 +31,35 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-6 col-lg-8 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">SELAMAT DATANG</h1>
-                                    </div>
-                                    <?= $this->session->flashdata('pesan'); ?>
-                                    <form class="user" method="post" action="<?= base_url('auth'); ?>">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Username">
-                                            <?= form_error('username', '<small class="text-warning ml-3">', '</small>') ?>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                                            <?= form_error('password', '<small class="text-warning ml-3">', '</small>') ?>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Masuk
-                                        </button>
-                                        <hr>
-                                    </form>
+                        <div class="col-lg-12">
+                            <div class="p-5">
+                                <div class="d-flex justify-content-center">
+                                    <img class="img-auth" src="<?= base_url('assets/img/icon/administration.png'); ?>" alt="">
                                 </div>
-                            </div>
-                            <div class="col-lg-6 d-none d-lg-block bg-login-img">
-                                <img src="<?= base_url('assets/img/auth/login.png'); ?>" alt="">
+                                <div class="text-center mt-2 mb-3">
+                                    <h1 class="h4 text-gray-900"><strong>SELAMAT DATANG</strong></h1>
+                                </div>
+                                <?= $this->session->flashdata('pesan'); ?>
+                                <form class="user" method="post" action="<?= base_url('auth'); ?>">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Masukan Username">
+                                        <?= form_error('username', '<small class="text-warning ml-3">', '</small>') ?>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Masukan Password">
+                                        <span toggle="#password" class="fa fa-fw fa-eye pass toggle-pass" data-toggle="tooltip" data-placement="top" title="Lihat Password"></span>
+                                        <?= form_error('password', '<small class="text-warning ml-3">', '</small>') ?>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Masuk
+                                    </button>
+                                    <hr>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -79,6 +80,9 @@
 
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+
+    <!-- javascript ex -->
+    <script src="<?= base_url('assets/'); ?>js/private.js"></script>
 
 </body>
 
