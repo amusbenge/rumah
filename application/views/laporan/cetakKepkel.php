8367b4c2a2ea13cd16ac9d55bcc2cf6fcc93af95
amosbenge <60687146+amosbenge@users.noreply.github.com>
2022-03-09 23:44:01 +0800

    Update cetakKepkel.php

--- application/views/laporan/cetakKepkel.php
+++ application/views/laporan/cetakKepkel.php
@@ -1,72 +1,62 @@
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>Laporan</title>
+    <title>Desain Tabel Menggunakan CSS</title>
     <style>
-        body {
+        .tabel1 {
             font-family: sans-serif;
-            font-size: 10pt;
+            color: #444;
+            border-collapse: collapse;
+            width: 100%;
+            border: 1px solid #f2f5f7;
         }
 
-        p {
-            margin: 0pt;
+        .tabel1 tr th {
+            background: #35A9DB;
+            color: #fff;
+            font-weight: normal;
         }
 
-        table.items {
-            border: 0.1mm solid #000000;
-        }
-
+        .tabel1,
+        th,
         td {
-            vertical-align: top;
+            padding: 10px 20px;
+            text-align: left;
         }
 
-        tr:nth-child(even) {
-            background-color: #ddd;
+        .tabel1 tr:hover {
+            background-color: #f5f5f5;
         }
 
-        .items td {
-            border-left: 0.1mm solid #000000;
-            border-right: 0.1mm solid #000000;
-        }
-
-        table thead td {
-            background-color: #4F8DF5;
-            text-align: center;
-            border: 0.1mm solid #000000;
-            font-variant: small-caps;
+        .tabel1 tr:nth-child(even) {
+            background-color: #f2f2f2;
         }
     </style>
 </head>
 
 <body>
-    <div class="" style="text-align: center; font-family: serif;">
-        <h2>Laporan Data Kepala Keluarga</h2>
-    </div>
-    <div style="text-align: left; font-family: serif;">Tanggal Cetak: <?= date('d F Y'); ?> </div><br><br>
-    <table class="items" width="100%" style="font-size: 9pt; font-family: serif; border-collapse: collapse; " cellpadding="8">
-        <thead>
+    <h1>lebakcyber.net</h1>
+    <table class="tabel1">
+        <tr>
+            <th width="5%">No</th>
+            <th width="10%">No KK</th>
+            <th width="10%">Kepala Keluarga</th>
+            <th width="10%">Alamat</th>
+            <th width="10%">Dusun</th>
+        </tr>
+        <!-- ITEMS HERE -->
+        <?php $i = 1; ?>
+        <?php foreach ($data['kepkel'] as $k) : ?>
             <tr>
-                <td width="5%">No</td>
-                <td width="10%">No KK</td>
-                <td width="10%">Kepala Keluarga</td>
-                <td width="10%">Alamat</td>
-                <td width="10%">Dusun</td>
+                <td scope="row"><?= $i; ?></td>
+                <td><?= $k['no_kk']; ?></td>
+                <td><?= $k['nm_kpl_kel']; ?></td>
+                <td><?= $k['alamat']; ?>, <?= $k['rt']; ?>/<?= $k['rw']; ?>, <?= $k['desa']; ?>, <?= $k['kec']; ?>, <?= $k['kab']; ?></td>
+                <td><?= $k['nama_dusun']; ?></td>
             </tr>
-        </thead>
-        <tbody>
-            <!-- ITEMS HERE -->
-            <?php $i = 1; ?>
-            <?php foreach ($data['kepkel'] as $k) : ?>
-                <tr>
-                    <th scope="row"><?= $i; ?></th>
-                    <td><?= $k['no_kk']; ?></td>
-                    <td><?= $k['nm_kpl_kel']; ?></td>
-                    <td><?= $k['alamat']; ?>, <?= $k['rt']; ?>/<?= $k['rw']; ?>, <?= $k['desa']; ?>, <?= $k['kec']; ?>, <?= $k['kab']; ?></td>
-                    <td><?= $k['nama_dusun']; ?></td>
-                </tr>
-                <?php $i++; ?>
-            <?php endforeach; ?>
-        </tbody>
+            <?php $i++; ?>
+        <?php endforeach; ?>
     </table>
 </body>
 
